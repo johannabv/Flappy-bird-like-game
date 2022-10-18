e6c237cfe8a763aef9a7cd14a964a9917aef3527
johannabv <99389409+johannabv@users.noreply.github.com>
2022-10-18 13:13:08 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Flappy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_planet4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_planet5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_planet3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_planet1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_planet2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_flappy = new System.Windows.Forms.PictureBox();
+            this.pictureBox_start = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_flappy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_start)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 528);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SCORE:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Flappy.Properties.Resources.heart;
+            this.pictureBox5.Location = new System.Drawing.Point(920, 516);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Flappy.Properties.Resources.heart;
+            this.pictureBox4.Location = new System.Drawing.Point(864, 516);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Flappy.Properties.Resources.heart;
+            this.pictureBox3.Location = new System.Drawing.Point(808, 516);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Flappy.Properties.Resources.heart;
+            this.pictureBox2.Location = new System.Drawing.Point(750, 516);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Flappy.Properties.Resources.heart;
+            this.pictureBox1.Location = new System.Drawing.Point(694, 516);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox_planet4
+            // 
+            this.pictureBox_planet4.Image = global::Flappy.Properties.Resources.galaxy;
+            this.pictureBox_planet4.Location = new System.Drawing.Point(750, 100);
+            this.pictureBox_planet4.Name = "pictureBox_planet4";
+            this.pictureBox_planet4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_planet4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_planet4.TabIndex = 6;
+            this.pictureBox_planet4.TabStop = false;
+            // 
+            // pictureBox_planet5
+            // 
+            this.pictureBox_planet5.Image = global::Flappy.Properties.Resources.planet_earth__1_;
+            this.pictureBox_planet5.Location = new System.Drawing.Point(950, 400);
+            this.pictureBox_planet5.Name = "pictureBox_planet5";
+            this.pictureBox_planet5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_planet5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_planet5.TabIndex = 5;
+            this.pictureBox_planet5.TabStop = false;
+            // 
+            // pictureBox_planet3
+            // 
+            this.pictureBox_planet3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_planet3.Image = global::Flappy.Properties.Resources.planets;
+            this.pictureBox_planet3.Location = new System.Drawing.Point(550, 400);
+            this.pictureBox_planet3.Name = "pictureBox_planet3";
+            this.pictureBox_planet3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_planet3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_planet3.TabIndex = 3;
+            this.pictureBox_planet3.TabStop = false;
+            // 
+            // pictureBox_planet1
+            // 
+            this.pictureBox_planet1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_planet1.Image = global::Flappy.Properties.Resources.planet_earth;
+            this.pictureBox_planet1.Location = new System.Drawing.Point(150, 350);
+            this.pictureBox_planet1.Name = "pictureBox_planet1";
+            this.pictureBox_planet1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_planet1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_planet1.TabIndex = 2;
+            this.pictureBox_planet1.TabStop = false;
+            // 
+            // pictureBox_planet2
+            // 
+            this.pictureBox_planet2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_planet2.Image = global::Flappy.Properties.Resources.planet;
+            this.pictureBox_planet2.Location = new System.Drawing.Point(350, 75);
+            this.pictureBox_planet2.Name = "pictureBox_planet2";
+            this.pictureBox_planet2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_planet2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_planet2.TabIndex = 1;
+            this.pictureBox_planet2.TabStop = false;
+            // 
+            // pictureBox_flappy
+            // 
+            this.pictureBox_flappy.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_flappy.Image = global::Flappy.Properties.Resources.fly;
+            this.pictureBox_flappy.Location = new System.Drawing.Point(12, 156);
+            this.pictureBox_flappy.Name = "pictureBox_flappy";
+            this.pictureBox_flappy.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_flappy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_flappy.TabIndex = 0;
+            this.pictureBox_flappy.TabStop = false;
+            // 
+            // pictureBox_start
+            // 
+            this.pictureBox_start.BackgroundImage = global::Flappy.Properties.Resources.start;
+            this.pictureBox_start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox_start.Location = new System.Drawing.Point(323, 427);
+            this.pictureBox_start.Name = "pictureBox_start";
+            this.pictureBox_start.Size = new System.Drawing.Size(205, 201);
+            this.pictureBox_start.TabIndex = 12;
+            this.pictureBox_start.TabStop = false;
+            this.pictureBox_start.Click += new System.EventHandler(this.pictureBox_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 578);
+            this.Controls.Add(this.pictureBox_start);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox_planet4);
+            this.Controls.Add(this.pictureBox_planet5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_planet3);
+            this.Controls.Add(this.pictureBox_planet1);
+            this.Controls.Add(this.pictureBox_planet2);
+            this.Controls.Add(this.pictureBox_flappy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_planet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_flappy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_start)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_flappy;
+        private System.Windows.Forms.PictureBox pictureBox_planet2;
+        private System.Windows.Forms.PictureBox pictureBox_planet1;
+        private System.Windows.Forms.PictureBox pictureBox_planet3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox_planet5;
+        private System.Windows.Forms.PictureBox pictureBox_planet4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox_start;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Flappy {
+    public partial class Form1 : Form {
+        private int planetSpeed = 8;
+        private int gravity = 5;
+        private int score = 0;
+        private int livesLost = 0;
+        private Random rand = new Random();
+        private List<PictureBox> galaxy;
+        private List<PictureBox> lives;
+        
+        public Form1() {
+            InitializeComponent();
+            galaxy = new List<PictureBox>()
+                {pictureBox_planet1, pictureBox_planet2, pictureBox_planet3, pictureBox_planet4, pictureBox_planet5};
+            lives = new List<PictureBox>() {pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5};
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e) {
+            pictureBox_flappy.Top += gravity;
+            
+            ControlColliding();
+            ChangePlanetsLocation();
+
+            if (livesLost==5) {
+                pictureBox5.Visible = false;
+                EndGame();
+            }
+        }
+
+        private void EndGame() {
+            timer1.Stop();
+            string message = "Your score: " + score + ". Do you want to play again?";
+            string caption = "Game over!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+            if (MessageBox.Show(message, caption, buttons) == DialogResult.Yes) ResetGame();
+            else Application.Exit();
+        }
+
+        private void ResetGame() {
+            score = 0;
+            livesLost = 0;
+            foreach (PictureBox life in lives) life.Visible = true;
+            timer1.Start();
+        }
+
+        private void ControlColliding() {
+            foreach (PictureBox planet in galaxy) {
+                if (pictureBox_flappy.Bounds.IntersectsWith(planet.Bounds) && planet.Visible==true) {
+                    livesLost++;
+                    if (livesLost < 5) lives[livesLost - 1].Visible = false;
+                    planet.Visible = false;
+                }
+            } 
+        }
+
+        private void ChangePlanetsLocation() {
+            foreach (PictureBox planet in galaxy) {
+                planet.Left -= planetSpeed;
+
+                if (planet.Left < -100) {
+                    planet.Left = ClientSize.Width + planet.Width * 2;
+                    planet.Top = rand.Next(0, ClientSize.Height - planet.Height - pictureBox1.Height);
+                    
+                    if (planet.Visible==true) {
+                        score++;
+                        label1.Text = "SCORE: " + score;
+                    }
+                    else {
+                        planet.Visible = true;
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Space) gravity = 5;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Space) gravity = -5;
+        }
+
+        private void pictureBox_start_Click(object sender, EventArgs e) {
+            pictureBox_start.Enabled = false;
+            pictureBox_start.Visible = false;
+            timer1.Interval = 20;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            timer1.Stop();
+        }
+    }
+}
